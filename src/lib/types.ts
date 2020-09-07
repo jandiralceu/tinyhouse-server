2a950decf3764a3a8d54144ce5922f495ad12077
Jandir A. Cutabiala <jandiralceu@gmail.com>
2020-09-07 01:46:34 -0300

    feat: create interfaces to collection

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,15 +1,57 @@
 import { ObjectID, Collection } from "mongodb";
 
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE",
+}
+
+export interface BookingIndexMonth {
+  [key: string]: boolean;
+}
+
+export interface BookingIndexYear {
+  [key: string]: BookingIndexMonth;
+}
+
+export interface BookingIndex {
+  [key: string]: BookingIndexYear
+}
+
 export interface Listing {
   _id: ObjectID;
+  title: string;
+  description: string;
+  image: string;
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectID[];
+  bookingsIndex: BookingIndex;
+  price: number;
+  numOfGuests: number;
 }
 
 export interface User {
-  _id: ObjectID
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletID?: string;
+  income: number;
+  bookings: ObjectID[];
+  listings: ObjectID[];
 }
 
 export interface Booking {
-  _id: ObjectID
+  _id: ObjectID;
+  listing: ObjectID;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface Database {
